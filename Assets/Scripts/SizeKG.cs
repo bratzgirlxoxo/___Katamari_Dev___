97038239888a54a926d1f5b1daf3e7627e094ab5
Kevin Garcia <kjg365@nyu.edu>
2018-11-06 16:03:30 -0500

    Greybox

---
+++ Assets/Scripts/SizeKG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Usage: Put this on an Object that can be picked up
+//Intent: This script helps determine if an object can be picked up
+//        and is reference for detemining the new size of the ball
+
+
+public class SizeKG : MonoBehaviour 
+{
+
+    // Variables:
+    public float mass;
+
+	
+	void Start () 
+    {
+        mass = 0f;
+	}
+	
+	
+}
