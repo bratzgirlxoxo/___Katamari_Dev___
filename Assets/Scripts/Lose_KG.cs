f96093a2872978880a36bfe070b8c8f8a10e32d3
Kevin Garcia <kjg365@nyu.edu>
2018-12-04 11:45:50 -0500

    Added Lose Script and started Lose Scene

---
+++ Assets/Scripts/Lose_KG.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Usage: Put this on a Game Manager in the Play scene
+// Intent: This will determine if the player has met the min size for victory and if not it will go to the lose scene
+
+public class Lose_KG : MonoBehaviour 
+{
+
+    public float minSize;
+
+    private PickUpStuff_ML ballsizescript;
+
+
+	void Start()
+	{
+        ballsizescript = GetComponent<PickUpStuff_ML>();
+	}
+
+
+	void Update () 
+    {
+        float ballsize = ballsizescript.ballSize; 
+		if (ballsize <= minSize)
+        {
+            SceneManager.LoadScene("Lose");
+        }
+	}
+}
---
+++ Assets/Scripts/TextChanger_KG.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// Usage: Put this on a Game Manager
+// Intent: Makes the text change when player clicks
+
+public class TextChanger_KG : MonoBehaviour 
+{
+    public string[] messages;
+    public int messageIndex = 0; 
+    public bool toggleMessages = true;
+	
+
+	void Update () 
+    {
+        if (Input.GetMouseButtonUp(0))  // Left Mouse Click to move forward one message
+            messageIndex++;
+        if (Input.GetMouseButtonUp(1))  // Right Mouse Click to move backward one message
+            messageIndex++;
+
+        // Reload Scene if we get to the last message
+        if (messageIndex > messages.GetUpperBound(0))
+        {
+            SceneManager.LoadScene("Level Prototype");
+        }
+
+        // Bounds checking
+         messageIndex = Mathf.Clamp(messageIndex, messages.GetLowerBound(0), messages.GetUpperBound(0)); 
+            
+
+	}
+
+    void OnGUI()
+    {
+        // This shows the Text on the UI
+        if (toggleMessages)
+            GUI.TextArea(new Rect(420, 250, 100, 100), messages[messageIndex]);
+    }
+}
