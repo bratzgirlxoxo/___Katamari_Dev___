4dd6876d57672b1ce79dd24e95e771b6749df928
trans-osaurus <38438534+trans-osaurus@users.noreply.github.com>
2018-12-19 20:36:59 -0500

    //added more models and UVs

---
+++ Assets/Scripts/AudioManagerOH.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManagerOH : MonoBehaviour {
+
+	//INTENT: Manages sound effects for the scene, play a sound effect when a object is picked up
+	//USAGE: Place on a empty game object in the scene hierarchy 
+
+	private static AudioManagerOH instance = null;
+
+	public AudioClip ChimeSound; //this is a placeholder sound effect, feel free to replace it with something!
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
